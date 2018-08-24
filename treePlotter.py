2fdbbb4653bcffe5161d1593842e90d8f593bd78
hauru <haurunis@outlook.com>
2018-08-24 12:32:28 +0800

    23/08-24/08 debugging

--- treePlotter.py
+++ treePlotter.py
@@ -1,9 +1,11 @@
 '''
-Created on Oct 14, 2010
+Created on Aug, 2018
 
-@author: Peter Harrington
+@author: Frederick Ziyang HONG
 '''
-import matplotlib.pyplot as plt
+import  matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
 
 decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
@@ -11,7 +13,8 @@ arrow_args = dict(arrowstyle="<-")
 
 def getNumLeafs(myTree):
     numLeafs = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())
+    firstStr = firstStr[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes
@@ -21,7 +24,8 @@ def getNumLeafs(myTree):
 
 def getTreeDepth(myTree):
     maxDepth = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())
+    firstStr = firstStr[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes
@@ -43,14 +47,15 @@ def plotMidText(cntrPt, parentPt, txtString):
 def plotTree(myTree, parentPt, nodeTxt):#if the first key tells you what feat was split on
     numLeafs = getNumLeafs(myTree)  #this determines the x width of this tree
     depth = getTreeDepth(myTree)
-    firstStr = myTree.keys()[0]     #the text label for this node should be this
+    firstStr = list(myTree.keys())     #the text label for this node should be this
+    firstStr = firstStr[0]
     cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
     plotMidText(cntrPt, parentPt, nodeTxt)
     plotNode(firstStr, cntrPt, parentPt, decisionNode)
     secondDict = myTree[firstStr]
     plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
     for key in secondDict.keys():
-        if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes   
+        if type(secondDict[key]).__name__=='dict':#test to see if the nodes are dictonaires, if not they are leaf nodes
             plotTree(secondDict[key],cntrPt,str(key))        #recursion
         else:   #it's a leaf node print the leaf node
             plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
@@ -64,17 +69,17 @@ def createPlot(inTree):
     fig.clf()
     axprops = dict(xticks=[], yticks=[])
     createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)    #no ticks
-    #createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses 
+    #createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses
     plotTree.totalW = float(getNumLeafs(inTree))
     plotTree.totalD = float(getTreeDepth(inTree))
     plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;
     plotTree(inTree, (0.5,1.0), '')
     plt.show()
 
-#def createPlot():
+# def createPlot():
 #    fig = plt.figure(1, facecolor='white')
 #    fig.clf()
-#    createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses 
+#    createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses
 #    plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
 #    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
 #    plt.show()
